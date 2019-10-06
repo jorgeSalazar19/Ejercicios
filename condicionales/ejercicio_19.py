2ee744a93fd4a216ef678b4a2df838ac5c13e2cf
jorge salazar <jsalazar@ikatechsolutions.com>
2019-10-06 16:57:52 -0500

    se modifica el ejercicio_19.py

--- condicionales/ejercicio_19.py
+++ condicionales/ejercicio_19.py
@@ -4,29 +4,17 @@
 num = int(input('Ingrese el primer numero: '))
 num_2 = int(input('Ingrese el segundo numero: '))
 
-if num < 100 or num > 999:
-    print('El primer numero no es de 3 digitos')
-else:
-    if num_2 < 100 or num_2 > 999:
-        print('El segundo numero no es de 3 digitos')
+if num > num_2:
+    num_2 = int(input('Ingrese el tercer numero: '))
+
+    if num > num_2:
+        print('El primer numero ingresado es el mayor')
     else:
-        if num > num_2:
-            num_2 = int(input('Ingrese el tercer numero: '))
+        print('El tercer numero ingresado es el mayor')
+else:
+    num = int(input('Ingrese el tercer numero: '))
 
-            if num_2 < 100 or num_2 > 999:
-                print('El tercer numero no es de 3 digitos')
-            else:
-                if num > num_2:
-                    print('El primer numero ingresado es el mayor')
-                else:
-                    print('El tercer numero ingresado es el mayor')
-        else:
-            num = int(input('Ingrese el tercer numero: '))
-
-            if num < 100 or num > 999:
-                print('El tercer numero no es de 3 digitos')
-            else:
-                if num_2 > num:
-                    print('El segundo numero ingresado es el mayor')
-                else:
-                    print('el tercer numero es el mayor')
+    if num_2 > num:
+        print('El segundo numero ingresado es el mayor')
+    else:
+        print('el tercer numero es el mayor')
